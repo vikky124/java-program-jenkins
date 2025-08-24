43ff3ee68002c664c204829457d2f455a1fc63ba
vikky124 <103567191+vikky124@users.noreply.github.com>
2025-08-24 16:22:45 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("This is java program");
+        System.out.println("TOutput from java program");
+        System.out.println(new Date());
+        System.out.println("hello");
+        System.out.println("world");
+
+    }
+}
