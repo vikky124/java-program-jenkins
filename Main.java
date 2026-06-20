f29dfc74857e5a4dbc8c33ff6c4c47f769e2b374
vikky124 <103567191+vikky124@users.noreply.github.com>
2026-06-20 14:01:36 +0530

    Update H-Q

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 import java.util.Date;
-public class Main{
-    public static void main(String[] args){
-        System.out.println("This is java program");
-        System.out.println("TOutput from java program");
-        System.out.println(new Date());
-        System.out.println("hello");
-        System.out.println("world");
-        System.out.println("added new file");
-        Student s=new Student();
-        s.sayWello();
 
-        
+public class Main {
+        public static void main(String[] args) {
+                System.out.println("This is java program");
+                System.out.println("TOutput from java program");
+                System.out.println(new Date());
+                System.out.println("hello");
+                System.out.println("world");
+                System.out.println("added new file");
+                Student s = new Student();
+                s.sayWello();
+                s.sayQello();
 
-    }
+        }
 }
